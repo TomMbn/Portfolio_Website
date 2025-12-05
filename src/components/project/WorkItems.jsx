b557fa03355b2db6a4913627c68f7a36daa2235b
TomMbn <tom.mauboussin@icloud.com>
2025-12-05 11:36:12 +0100

    fix : Accessibilité

--- src/components/project/WorkItems.jsx
+++ src/components/project/WorkItems.jsx
@@ -6,7 +6,7 @@ const openModal = (item) => {
 const WorkItems = ({item}) => {
   return (
     <div className="workCard" key={item.id} onClick={()=> openModal(item)}>
-        <img src={item.image} alt={item.title + " work image"} className="workImg"/>
+        <img src={item.image} alt={item.title + " work screenshot"} className="workImg"/>
         <h3 className="workTitle">{item.title}</h3>
         <a href={item.github} className="workButton" target="_blank" rel="noopener noreferrer">
           Source code for {item.title} on GitHub <i className='bx bx-right-arrow-alt workButtonIcon'></i>
